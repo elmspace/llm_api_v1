50fe4f59bf96da521eb72f95181a77c98797f325
Ash D <ash.dehghan@gmail.com>
2024-02-25 22:11:04 -0500

    Cleaning up the code

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-import json
-import pandas as pd
 from flask import Flask
 from flask import request
-from flask import Response
-from cachetools import TTLCache
-from flask import make_response
 
 # Internal Modules
 from modules.summarizer import Summarizer
@@ -12,14 +7,12 @@ from modules.summarizer import Summarizer
 smrz = Summarizer()
 
 app = Flask(__name__)
-cache = TTLCache(maxsize=500, ttl=60)
 
 @app.route("/summarize_text", methods=["POST"])
 def summarize_text():
 	req = request.get_json()
 	res = smrz.run(req)
 	return res
-
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5079, threaded=True, debug=True)
